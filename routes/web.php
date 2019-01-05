17296d077309f9e5a2ede00b5e1d0ec76b06eeed
Aris Arisandi <arris.smpn6@gmail.com>
2019-01-05 21:10:57 +0700

    Starter Pack API & Web Backend Laravel

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+/* Send Email Route Queue */
+Route::get('send-email', function(){
+  
+	$details['email'] = 'arris.smpn6@gmail.com';
+  
+    dispatch(new App\Jobs\SendEmailJob($details));
+  
+    dd('done');
+});
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes(["verify" => true]);
+
+Route::get('/home', 'HomeController@index')->name('home')->middleware('verified');
