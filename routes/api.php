6e8b837a7195296116fdcbacaaa58054321ee609
idnasirasira <aris.arisandi17@gmail.com>
2019-01-05 22:20:02 +0700

    Add Route for Post

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,8 @@ use Illuminate\Http\Request;
 */
 
 Route::post('register', 'API\RegisterController@register');
-  
+
 Route::middleware('auth:api')->group( function () {
 	Route::resource('products', 'API\ProductController');
+	Route::resource('post', 'API\PostController');
 });
